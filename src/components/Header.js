a8e95ba334659cb117c96e558816d7aa1c0c6d9a
Lam Nguyen <lamnguyenttk50@gmail.com>
2023-07-25 23:59:59 +0700

    Call Api Users

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,9 @@ function Header({ props }) {
             {' '}
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand className="title" href="#home">
+                        conduit
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
--- src/components/TableUses.js
+++ src/components/TableUses.js
@@ -1,5 +1,50 @@
+import Table from 'react-bootstrap/Table';
+import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { fetchAllUser } from '../service/UseService';
+
 function TableUses({ props }) {
-    return <>TableUses...</>;
+    const [listUsers, setListUsers] = useState([]);
+
+    useEffect(() => {
+        // call api
+        getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        let res = await fetchAllUser();
+
+        if (res && res.data && res.data.data) {
+            setListUsers(res.data.data);
+        }
+    };
+    console.log(listUsers);
+    return (
+        <Container>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers &&
+                        listUsers.length > 0 &&
+                        listUsers.map((item, index) => (
+                            <tr key={`users-${index}`}>
+                                <td>{item.id}</td>
+                                <td>{item.email}</td>
+                                <td>{item.first_name}</td>
+                                <td>{item.last_name}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </Table>
+        </Container>
+    );
 }
 
 export default TableUses;
