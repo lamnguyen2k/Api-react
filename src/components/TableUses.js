59873127de2abe321606bc9bbc3d59480a7378c0
Lam Nguyen <lamnguyenttk50@gmail.com>
2023-07-26 00:26:00 +0700

    customize-axios

--- src/components/TableUses.js
+++ src/components/TableUses.js
@@ -14,11 +14,11 @@ function TableUses({ props }) {
     const getUsers = async () => {
         let res = await fetchAllUser();
 
-        if (res && res.data && res.data.data) {
-            setListUsers(res.data.data);
+        if (res && res.data) {
+            setListUsers(res.data);
         }
     };
-    console.log(listUsers);
+
     return (
         <Container>
             <Table striped bordered hover>
